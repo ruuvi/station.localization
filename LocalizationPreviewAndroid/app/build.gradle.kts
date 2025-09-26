13dc4961c95bfd98b6a9f11735efb4381e742eae
Denis Andreev <andreev.denis@gmail.com>
2025-09-26 20:30:03 +0300

    Localization preview tool Libs clean up

--- LocalizationPreviewAndroid/app/build.gradle.kts
+++ LocalizationPreviewAndroid/app/build.gradle.kts
@@ -49,13 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    testImplementation(libs.junit)
     implementation(libs.commons.text)
 
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
